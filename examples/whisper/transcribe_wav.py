28d26a4652fe470f8b084b2b70a7943e91119dbe
Manjunath Kudlur <keveman@usefulsensors.com>
2023-08-28 14:12:48 -0500

    Update dependencies

--- examples/whisper/transcribe_wav.py
+++ examples/whisper/transcribe_wav.py
@@ -6,7 +6,7 @@ from .whisper import decode_wav_file
 
 def main():
     if len(sys.argv) < 2:
-        wav_file = os.path.join(os.path.dirname(__file__), 'assets', 'asknot.wav')
+        wav_file = os.path.join(os.path.dirname(__file__), 'assets', 'ever_tried.wav')
     else:
         wav_file = sys.argv[1]
     text = decode_wav_file(wav_file)
